9f46c1d5d655a6ae1ae80febbb2ce181635be15f
tbvjaos510 <tbvjaos510@naver.com>
2018-10-11 17:50:51 +0900

    [ADD] Interaction.vue 와 연동

--- socket/room.js
+++ socket/room.js
@@ -1,18 +1,35 @@
+var interActions = {}
+
 /**
  * Socket.io 라우팅
  * @param {SocketIO.Socket} client
  */
 function Route(client) {
     console.log('Connect', client.id)
-    client.on('join', function(data) {
+    client.on('join', function(data, cb) {
         client.leaveAll()
         client.join(data.name)
+        console.log('join', data.name)
+        if (!interActions[data.name])
+            interActions[data.name] = []
         client.nowRoom = data.name
+        cb(interActions[data.name])
     })
     client.on('upload', function(data) {
         if (client.nowRoom)
+        {
+            interActions[client.nowRoom].push(data)
             return client.to(client.nowRoom).broadcast.emit('upload', data) 
-       else
+        }
+        else
+            return client.emit('errors', "Room Not Connect")
+    })
+    client.on('deleteAll', function () {
+        if (client.nowRoom) {
+            client.to(client.nowRoom).emit('deleteAll')
+            interActions[client.nowRoom] = []
+        }
+        else
             return client.emit('errors', "Room Not Connect")
     })
 }
