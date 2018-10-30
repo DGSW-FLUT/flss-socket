082706d65e9c9eaf847fb14662ae2c9466f66764
tbvjaos510 <tbvjaos510@naver.com>
2018-10-31 08:49:00 +0900

    [ADD] DB 구조 변경 및 아이템 하나 삭제 구현

--- socket/room.js
+++ socket/room.js
@@ -4,12 +4,6 @@ const sequelize = new Sequelize(sqlInfo.database, sqlInfo.user, sqlInfo.password
   host: sqlInfo.host,
   dialect: "mysql"
 })
-const Interaction = sequelize.define('Interaction', {
-    Aid: {type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true},
-    Topic: {type: Sequelize.STRING(60), allowNull: false},
-    Link: {type: Sequelize.TEXT, allowNull: false},
-    Cid: {type: Sequelize.INTEGER, allowNull: false, references: {model: 'Class', key: 'Cid'}}
-})
 /*
 +----------+---------+------+-----+---------+----------------+
 | Field    | Type    | Null | Key | Default | Extra          |
@@ -24,10 +18,14 @@ const Interaction = sequelize.define('Interaction', {
 const Interactionitems = sequelize.define('InteractionItems', {
     Iid: {type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true},
     Aid: {type: Sequelize.INTEGER, references: {model: 'Interaction', key: 'Aid'}},
-    Title: {type: Sequelize.TEXT, allowNull: true, defaultValue: null},
-    Contents: {type: Sequelize.TEXT, allowNull: true, defaultValue: null},
-    Mid: {type: Sequelize.INTEGER, allowNull: false, references: {model: 'Cloud', key: 'Mid'},}
-})
+    Topic: {type: Sequelize.TEXT, allowNull: true, defaultValue: null},
+    Content: {type: Sequelize.TEXT, allowNull: true, defaultValue: null},
+    file: {type: Sequelize.TEXT, allowNull: true, defaultValue: null},
+    Link: {type: Sequelize.TEXT, allowNull: true ,defaultValue: null} 
+    }, {
+        timestamps: false
+    }
+)
 // Interactionitems.belongsTo(Interaction, {foreignKey: 'Aid', targetKey: 'Aid'})
 var interActions = {}
 
@@ -44,15 +42,24 @@ function Route(client) {
         if (!interActions[data.name])
             interActions[data.name] = []
         client.nowRoom = data.name
-        cb(interActions[data.name])
+        // cb(interActions[data.name])
+        Interactionitems.findAll({
+            where: {
+                Aid: data.name
+            }
+        })
+            .then((res) => {
+                cb(res)    
+            })
     })
     client.on('upload', function(data) {
         if (client.nowRoom)
         {
+            console.log(data)
             // interActions[client.nowRoom].push(data)
-            Interactionitems.create({Aid: client.newRoom, Mid: data.Mid, Title: data.title, Contents: data.content})
+            Interactionitems.create({Aid: client.nowRoom, file: data.file, Topic: data.Topic, Content: data.Content, Link: data.Link})
                 .then((value) => {
-                    return client.to(client.newRoom).broadcast.emit('upload', data)
+                    return client.to(client.nowRoom).broadcast.emit('upload', data)
                 })
                 .catch((err) => {
                     console.error(err)
@@ -62,15 +69,29 @@ function Route(client) {
         else
             return client.emit('errors', "Room Not Connect")
     })
+    client.on('delete', function(data, cb) {
+        if (client.nowRoom) {
+            console.log(data)
+            Interactionitems.destroy({where: { Iid: data } })
+                .then((value) => {
+                    client.to(client.nowRoom).broadcast.emit('delete', data)
+                    return cb({success: true})
+                })
+                .catch((err) => console.error(err))
+
+        }
+        else
+            return client.emit('errors', "Room Not Connect")
+    })
     client.on('deleteAll', function () {
         if (client.nowRoom) {
             Interactionitems.destroy({
                 where: {
-                    Aid: client.newRoom
+                    Aid: client.nowRoom
                 }
             }).then((value) => {
                 return client.to(client.nowRoom).emit('deleteAll')
-            }).err((err) => {
+            }).catch((err) => {
                 return client.emit('errors', 'delete all error')
             })
             // interActions[client.nowRoom] = []
